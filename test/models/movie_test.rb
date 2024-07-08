4f09bf1d106ff166045f9ea0c3f018ac4de7bb03
Trunganh <trungvdh@its-global.vn>
2024-07-08 22:23:01 +0700

    build project

---
+++ test/models/movie_test.rb
@@ -0,0 +1,22 @@
+# == Schema Information
+#
+# Table name: movies
+#
+#  id               :bigint           not null, primary key
+#  description      :text(65535)
+#  thumb_url        :string(255)      not null
+#  title            :string(255)      not null
+#  vote_downs_count :bigint           default(0), not null
+#  vote_ups_count   :bigint           default(0), not null
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#  user_id          :bigint           not null
+#  video_id         :string(255)      not null
+#
+require "test_helper"
+
+class MovieTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
